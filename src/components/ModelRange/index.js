d5e2ae6415b86a05902f4c85a1d16d04882dd978
Edin Delan <edindelan@Edins-MacBook-Pro.local>
2016-12-28 12:56:52 +0100

    Model battery range component

---
+++ src/components/ModelRange/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Stats from './stats';
+import './model-range.css';
+
+export default class ModelRange extends Component {
+    render() {
+        const m = this.props.model,
+              w = this.props.state.wheel,
+              c = this.props.state.climate,
+              s = this.props.state.speed,
+              t = this.props.state.temperature;
+
+        const range = Stats.getModelData()[m][w][c]['speed'][s][t];
+
+        return (
+            <div className="model-range-item">
+                <div className={'model-number model-' + m}></div>
+                <div className="range">{range}</div>
+            </div>
+        )
+    }
+}
